73e3ff282856cb1576290602f1366ad06051868a
banjun <banjun@gmail.com>
2024-01-31 18:27:17 +0900

    experiment for safari tab overview ui

--- RemixSwiftUI/RemixSwiftUIApp.swift
+++ RemixSwiftUI/RemixSwiftUIApp.swift
@@ -27,6 +27,7 @@ struct RemixSwiftUIApp: SwiftUI.App {
             Popup(offsetY: { $0 ? $1.convert(-5, from: .centimeters) : 0 }) { Text("Open Toggle").font(.extraLargeTitle) } content: {
                 Toggle("Toggle", isOn: $toggleValue).fixedSize().padding()
             },
+            SafarishTabOverview(),
             Text("Text 1").font(.extraLargeTitle).padding().glassBackgroundEffect(),
             Text("Text 2").font(.extraLargeTitle).padding().glassBackgroundEffect(),
             Text("Text 3").font(.extraLargeTitle).padding().glassBackgroundEffect(),
@@ -62,6 +63,75 @@ struct RemixSwiftUIApp: SwiftUI.App {
                 await dismissImmersiveSpace()
                 immersiveSpaceIsShown = false
             }
+        }
+    }
+}
+
+struct SafarishTabOverview: View {
+    @State private var showsOverview: Bool = false
+    private let cells: [any View] = [
+        Text("Cell").font(.extraLargeTitle).fixedSize().padding(EdgeInsets(top: 50, leading: 150, bottom: 50, trailing: 150)),
+        Text("Cell").font(.extraLargeTitle).fixedSize().padding(EdgeInsets(top: 50, leading: 150, bottom: 50, trailing: 150)),
+        Text("Cell").font(.extraLargeTitle).fixedSize().padding(EdgeInsets(top: 50, leading: 150, bottom: 50, trailing: 150)),
+        Text("Cell").font(.extraLargeTitle).fixedSize().padding(EdgeInsets(top: 50, leading: 150, bottom: 50, trailing: 150)),
+        Text("Cell").font(.extraLargeTitle).fixedSize().padding(EdgeInsets(top: 50, leading: 150, bottom: 50, trailing: 150)),
+        Text("Cell").font(.extraLargeTitle).fixedSize().padding(EdgeInsets(top: 50, leading: 150, bottom: 50, trailing: 150)),
+        Text("Cell").font(.extraLargeTitle).fixedSize().padding(EdgeInsets(top: 50, leading: 150, bottom: 50, trailing: 150)),
+    ]
+    private var rows: [[any View]] {
+        stride(from: 0, to: cells.count, by: 3).map { Array(cells[$0..<min($0+3, cells.count)]) }
+    }
+
+    var body: some View {
+        Button("SafarishTabOverview") {
+            showsOverview = true
+        }
+        .font(.extraLargeTitle)
+
+        // NOTE: Safari Tab Overview style is difficult for mimic
+        // - present pages with clear background: .sheet nor .fullScreenCover in visionOS cannot clear background using .presentationBackground(.clear)
+        // - transition animation between the current page and page cells: how do we can this if transition is between Window <-> Immersive Space?
+        // - non-clipped tab overview: plain window tightly clips its content...
+        // - non-hiding other apps in the shared space: something different from nominal immersive...
+        // - dimming (non-accumulated): slightly dim environment, but 2 Safari windows do not dim 2 step of depth. simply dim on showing overview and un-dim on going back from the overview
+
+        //        if showsOverview {
+//        .fullScreenCover(isPresented: $showsOverview) {
+//            //        .sheet(isPresented: $showsOverview) {
+//        }
+//        .presentationBackground(.clear)
+        if !showsOverview {
+            if let cell = cells.first {
+                AnyView(cell.id(0))
+                    .glassBackgroundEffect()
+            }
+        } else {
+            RealityView { content, attachments in
+            } update: { content, attachments in
+                guard let e = attachments.entity(for: "x") else { return }
+                guard e.parent == nil else { return }
+                content.add(e)
+            } attachments: {
+                Attachment(id: "x") {
+                    Button("Close") { withAnimation(.spring) { showsOverview = false } } // TODO: toolbar?
+                    Spacer().frame(height: 44)
+                    ScrollView(.vertical) {
+                        Grid(alignment: .topLeading, horizontalSpacing: 44, verticalSpacing: 44) {
+                            ForEach(Array(rows.enumerated()), id: \.offset) { _, row in
+                                GridRow(alignment: .top) {
+                                    ForEach(Array(row.enumerated()), id: \.offset) { offset, cell in
+                                        AnyView(cell.id(offset))
+                                            .glassBackgroundEffect()
+                                            .rotation3DEffect(.init(radians: -(CGFloat(offset) / CGFloat(rows.count - 1) - 0.5) * .pi / 6), axis: (0, 1, 0))
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+//            .frame(width: 1024, height: 1024)
+//            .frame(depth: 1024)
         }
     }
 }
